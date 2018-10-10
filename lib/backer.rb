fa15de40f84955776cf9790ef7a96ac5968f29bb
Merline Belizaire <merline@noukod.com>
2018-10-10 18:41:35 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,7 +1,7 @@
 class Backer
-  def initialize(name, back_project)
+  def initialize(name, backed_projects)
     @name = name
-    @back_project = back_project
+    @backed_projects = []
   end
 
 end
